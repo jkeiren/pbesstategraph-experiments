13679a591b8fc3ff46763b2b1e1a86e1c1cde42e
Jeroen Keiren <j.j.a.keiren@gmail.com>
2014-04-24 12:19:36 +0200

    Compress table

--- utilities/papertable.py
+++ utilities/papertable.py
@@ -21,19 +21,24 @@ def printsize(sizes, tool, percentages):
   else:
       return addsep(sizes[tool])
 
-def printtime(times, tool, percentages):
+def printtime(times, tool, percentages, msecs=False):
   if percentages and tool != 'original' and times['original'] != 'unknown' and times[tool] != 'unknown':
     return '{0:.2f}\%'.format((times[tool]/times['original']) * 100)
   elif times[tool] in ['unknown', 'timeout']:
     return times[tool]
   else:
     secs = int(times[tool])
-    msecs = int((times[tool] - secs)*100)
-    msecsstr = str(msecs)[:2]
-    if len(msecsstr) == 1:
-      msecsstr += '0'
     res = datetime.timedelta(seconds = secs)
-    return '{0}.{1}'.format(res, msecsstr)
+    
+    if msecs:
+      msecs = int((times[tool] - secs)*100)
+      msecsstr = str(msecs)[:2]
+      if len(msecsstr) == 1:
+        msecsstr += '0'
+      return '{0}.{1}'.format(res, msecsstr)
+
+    else:
+      return str(res)
   
 
 def getrow(data, case, property, reportsizes, reporttimes, reportsolution, percentages):
@@ -78,13 +83,13 @@ def getrow(data, case, property, reportsizes, reporttimes, reportsolution, perce
       times[tool] = instantiation + reduction + solving
 
   if reportsizes:
-    result += ' && {0} && {1} && {2} && {3}'.format(printsize(sizes, 'original', percentages),
+    result += ' & {0} & {1} & {2} & {3}'.format(printsize(sizes, 'original', percentages),
       printsize(sizes, 'pbesparelm', percentages),
       printsize(sizes, 'pbesstategraph (global)', percentages),
       printsize(sizes, 'pbesstategraph (local)', percentages))
 
   if reporttimes:
-    result += ' && {0} && {1} && {2} && {3}'.format(printtime(times, 'original', percentages),
+    result += ' & {0} & {1} & {2} & {3}'.format(printtime(times, 'original', percentages),
       printtime(times, 'pbesparelm', percentages),
       printtime(times, 'pbesstategraph (global)', percentages),
       printtime(times, 'pbesstategraph (local)', percentages))
@@ -108,7 +113,7 @@ def getrow(data, case, property, reportsizes, reporttimes, reportsolution, perce
     else:
       solstr = sol
 
-    result += ' && ${0}$'.format(solstr)
+    result += ' & ${0}$'.format(solstr)
     
   return '{0} \\\\'.format(result)
  
@@ -131,37 +136,37 @@ def gettable(data, outfilename, sizes, times, solution, percentages):
   texfile.write('''\\centering
 \\scriptsize
 ''')
-  texfile.write('\\begin{{tabular}}{{@{{}}llr{0}{1}{2}@{{}}}}'.format('crcrcrcrr' if sizes else '', 'crcrcrcrr' if times else '', 'cr' if solution else ''))
+  texfile.write('\\begin{{tabular}}{{lc@{{\\hspace{{5pt}}}}|@{{\\hspace{{5pt}}}}{0}{1}{2}@{{}}}}'.format('rrrr' if sizes else '', '@{\\hspace{5pt}}|@{\\hspace{5pt}}rrrr' if times else '', '@{\\hspace{5pt}}|@{\\hspace{5pt}}c' if solution else ''))
 #  texfile.write(
 
-  texfile.write('\\phantom{{AB}} & & \\phantom{{blablabla}} {0}{1}{2}\\\\'.format('& \\phantom{blabla} & Original & & \\texttt{parelm} & & \\texttt{stategraph} & & \\texttt{stategraph}' if sizes else '',
-    '& \\phantom{blabla} & Original & & \\texttt{parelm} & & \\texttt{stategraph} & & \\texttt{stategraph}' if times else '',
-    ' & & verdict' if solution else ''))
-  texfile.write('             & &                       {0}{1}{2}\\\\'.format('&&&&&& \\texttt{(global)}  & & \\texttt{(local)} ' if sizes else '', '&&&&&& \\texttt{(global)}  & & \\texttt{(local)}' if times else '', '&& ' if solution else ''))
+  texfile.write(' & $|D|$ {0}{1}{2}\\\\'.format(' & Original & \\texttt{parelm} & \\texttt{st.graph} & \\texttt{st.graph}' if sizes else '',
+    ' & Original & \\texttt{parelm} & \\texttt{st.graph} & \\texttt{st.graph}' if times else '',
+    ' & verdict' if solution else ''))
+  texfile.write('             &   {0}{1}{2}\\\\'.format('&&& \\texttt{(global)}  & \\texttt{(local)} ' if sizes else '', '&&& \\texttt{(global)}  & \\texttt{(local)}' if times else '', '& ' if solution else ''))
   texfile.write('''
 \\\\[-1ex]
 \\toprule
 \\\\[-1ex]
-& \\multicolumn{5}{c}{Model Checking Problems} \\\\[-1ex]
- \\cmidrule{2-6}  \\\\[-1ex]
+ \\multicolumn{4}{c}{Model Checking Problems} \\\\
+ \\cmidrule{1-4}  \\\\[-1ex]
 
 \\multicolumn{11}{l}{\\textbf{No deadlock}}  \\\\
 ''')
   
-  texfile.write('  & \\emph{Onebit}   & $|D| = 2$')
+  texfile.write('\\emph{Onebit}   & $2$')
   texfile.write(getrow(data, 'Onebit [datasize=2]', 'nodeadlock', sizes, times, solution, percentages))
   texfile.write('\n')
-  texfile.write('  &                 & $|D| = 3$')
+  texfile.write('                 & $3$')
   texfile.write(getrow(data, 'Onebit [datasize=3]', 'nodeadlock', sizes, times, solution, percentages))
   texfile.write('\n')
-  texfile.write('  &                 & $|D| = 4$')
+  texfile.write('                 & $4$')
   texfile.write(getrow(data, 'Onebit [datasize=4]', 'nodeadlock', sizes, times, solution, percentages))
   texfile.write('\n')
   
-  texfile.write('  & \\emph{Hesselink}   & $|D| = 2$')
+  texfile.write('\\emph{Hesselink} & $2$')
   texfile.write(getrow(data, 'Hesselink [datasize=2]', 'nodeadlock', sizes, times, solution, percentages))
   texfile.write('\n')
-  texfile.write('  &                 & $|D| = 3$')
+  texfile.write('                  & $3$')
   texfile.write(getrow(data, 'Hesselink [datasize=3]', 'nodeadlock', sizes, times, solution, percentages))
   texfile.write('\n')
   
@@ -169,13 +174,13 @@ def gettable(data, outfilename, sizes, times, solution, percentages):
 \\multicolumn{11}{l}{\\textbf{No spontaneous generation of messages}}  \\\\
 ''')
 
-  texfile.write('  & \\emph{Onebit}   & $|D| = 2$')
+  texfile.write('\\emph{Onebit}  & $2$')
   texfile.write(getrow(data, 'Onebit [datasize=2]', 'no_spontaneous_messages', sizes, times, solution, percentages))
   texfile.write('\n')
-  texfile.write('  &                 & $|D| = 3$')
+  texfile.write('                & $3$')
   texfile.write(getrow(data, 'Onebit [datasize=3]', 'no_spontaneous_messages', sizes, times, solution, percentages))
   texfile.write('\n')
-  texfile.write('  &                 & $|D| = 4$')
+  texfile.write('                & $4$')
   texfile.write(getrow(data, 'Onebit [datasize=4]', 'no_spontaneous_messages', sizes, times, solution, percentages))
   texfile.write('\n')
 
@@ -183,13 +188,13 @@ def gettable(data, outfilename, sizes, times, solution, percentages):
 \\\\[-1ex]
 \\multicolumn{11}{l}{\\textbf{Messages that are read are inevitably sent}}  \\\\
 ''')
-  texfile.write('  & \\emph{Onebit}   & $|D| = 2$')
+  texfile.write('\\emph{Onebit}  & $2$')
   texfile.write(getrow(data, 'Onebit [datasize=2]', 'messages_read_are_inevitably_sent', sizes, times, solution, percentages))
   texfile.write('\n')
-  texfile.write('  &                 & $|D| = 3$')
+  texfile.write('                & $3$')
   texfile.write(getrow(data, 'Onebit [datasize=3]', 'messages_read_are_inevitably_sent', sizes, times, solution, percentages))
   texfile.write('\n')
-  texfile.write('  &                 & $|D| = 4$')
+  texfile.write('                & $4$')
   texfile.write(getrow(data, 'Onebit [datasize=4]', 'messages_read_are_inevitably_sent', sizes, times, solution, percentages))
   texfile.write('\n')    
 
@@ -197,13 +202,13 @@ def gettable(data, outfilename, sizes, times, solution, percentages):
 \\\\[-1ex]
 \\multicolumn{11}{l}{\\textbf{Messages can overtake one another}}  \\\\
 ''')
-  texfile.write('  & \\emph{Onebit}   & $|D| = 2$')
+  texfile.write('\\emph{Onebit} & $2$')
   texfile.write(getrow(data, 'Onebit [datasize=2]', 'messages_can_be_overtaken', sizes, times, solution, percentages))
   texfile.write('\n')
-  texfile.write('  &                 & $|D| = 3$')
+  texfile.write('               & $3$')
   texfile.write(getrow(data, 'Onebit [datasize=3]', 'messages_can_be_overtaken', sizes, times, solution, percentages))
   texfile.write('\n')
-  texfile.write('  &                 & $|D| = 4$')
+  texfile.write('               & $4$')
   texfile.write(getrow(data, 'Onebit [datasize=4]', 'messages_can_be_overtaken', sizes, times, solution, percentages))
   texfile.write('\n')  
 
@@ -212,35 +217,35 @@ def gettable(data, outfilename, sizes, times, solution, percentages):
 \\multicolumn{11}{l}{\\textbf{Values written to the register can be read}} \\\\
 ''')
 
-  texfile.write('  & \\emph{Hesselink}   & $|D| = 2$')
+  texfile.write(' \\emph{Hesselink} & $2$')
   texfile.write(getrow(data, 'Hesselink [datasize=2]', 'property2', sizes, times, solution, percentages))
   texfile.write('\n')
-  texfile.write('  &                 & $|D| = 3$')
+  texfile.write('                 & $3$')
   texfile.write(getrow(data, 'Hesselink [datasize=3]', 'property2', sizes, times, solution, percentages))
   texfile.write('\n')
   
   texfile.write('''\\\\[-1ex]
-& \\multicolumn{5}{c}{Equivalence Checking Problems} \\\\
- \\cmidrule{2-6}  \\\\[-1ex]
+ \\multicolumn{4}{c}{Equivalence Checking Problems} \\\\
+ \\cmidrule{1-4}  \\\\[-1ex]
 
 \\multicolumn{11}{l}{\\textbf{Branching bisimulation equivalence}} \\\\
 ''')
 
-  texfile.write('  &  \\emph{ABP-CABP} & $|D| = 2$')
+  texfile.write('\\emph{ABP-CABP} & $2$')
   texfile.write(getrow(data, 'ABP/CABP (datasize=2 capacity=1 windowsize=1)', 'branching-bisim', sizes, times, solution, percentages))
   texfile.write('\n')
-  texfile.write('  &                  & $|D| = 4$')
+  texfile.write('                 & $4$')
   texfile.write(getrow(data, 'ABP/CABP (datasize=4 capacity=1 windowsize=1)', 'branching-bisim', sizes, times, solution, percentages))
   texfile.write('\n')
   
-  texfile.write('  &  \\emph{Buf-Onebit} & $|D| = 2$')
+  texfile.write('\\emph{Buf-Onebit} & $2$')
   texfile.write(getrow(data, 'Buffer/Onebit (datasize=2 capacity=2 windowsize=1)', 'branching-bisim', sizes, times, solution, percentages))
   texfile.write('\n')
-  texfile.write('  &                  & $|D| = 4$')
+  texfile.write('                   & $4$')
   texfile.write(getrow(data, 'Buffer/Onebit (datasize=4 capacity=2 windowsize=1)', 'branching-bisim', sizes, times, solution, percentages))
   texfile.write('\n')
   
-  texfile.write('  & \\emph{Hesselink I-S} & $|D| = 2$')
+  texfile.write('\\emph{Hesselink I-S} & $2$')
   texfile.write(getrow(data, 'Hesselink (Implementation)/Hesselink (Specification) (datasize=2)', 'branching-bisim', sizes, times, solution, percentages))
   texfile.write('\n')
   
@@ -249,21 +254,21 @@ def gettable(data, outfilename, sizes, times, solution, percentages):
 \\multicolumn{11}{l}{\\textbf{Weak bisimulation equivalence}}  \\\\
 ''')
 
-  texfile.write('  &  \\emph{ABP-CABP} & $|D| = 2$')
+  texfile.write('\\emph{ABP-CABP} & $2$')
   texfile.write(getrow(data, 'ABP/CABP (datasize=2 capacity=1 windowsize=1)', 'weak-bisim', sizes, times, solution, percentages))
   texfile.write('\n')
-  texfile.write('  &                  & $|D| = 4$')
+  texfile.write('                 & $4$')
   texfile.write(getrow(data, 'ABP/CABP (datasize=4 capacity=1 windowsize=1)', 'weak-bisim', sizes, times, solution, percentages))
   texfile.write('\n')
   
-  texfile.write('  &  \\emph{Buf-Onebit} & $|D| = 2$')
+  texfile.write('\\emph{Buf-Onebit} & $2$')
   texfile.write(getrow(data, 'Buffer/Onebit (datasize=2 capacity=2 windowsize=1)', 'weak-bisim', sizes, times, solution, percentages))
   texfile.write('\n')
-  texfile.write('  &                  & $|D| = 4$')
+  texfile.write('                   & $4$')
   texfile.write(getrow(data, 'Buffer/Onebit (datasize=4 capacity=2 windowsize=1)', 'weak-bisim', sizes, times, solution, percentages))
   texfile.write('\n')
   
-  texfile.write('  & \\emph{Hesselink I-S} & $|D| = 2$')
+  texfile.write('\\emph{Hesselink I-S} & $2$')
   texfile.write(getrow(data, 'Hesselink (Implementation)/Hesselink (Specification) (datasize=2)', 'weak-bisim', sizes, times, solution, percentages))
   texfile.write('\n')
 
